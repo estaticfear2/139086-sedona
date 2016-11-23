73717945ad22af7e39741e1ed69beabf41bb1c12
estaticfear2 <aphelion.unet@gmail.com>
2016-11-23 21:09:32 +0200

    7
    1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".search-form");
+var datein = popup.querySelector("#date-in");
+var dateout = popup.querySelector("#date-out");
+var grown = popup.querySelector("#grown");
+var children = popup.querySelector("#children");
+var storage_grown = localStorage.getItem("grown");
+var storage_children = localStorage.getItem("children");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();  
+  popup.classList.add("search-form-show");
+  if (storage_grown) {
+    grown.value = storage_grown;
+  }
+  if (storage_children) {
+    children.value = storage_children;
+  }
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("search-form-show")) {
+      popup.classList.remove("search-form-show");
+    }
+  }
+});
+
+window.addEventListener("submit", function(event) {
+  if (!datein.value || !dateout.value || !grown.value ||!children.value) {
+    event.preventDefault();
+  } else {
+          localStorage.setItem("grown", grown.value);
+          localStorage.setItem("children", children.value);
+          }
+});
